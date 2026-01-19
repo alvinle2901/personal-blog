d74f15d2b9b259803027ac9f4828c5e9e94c8e83
alvinle29 <bach938@gmail.com>
2026-01-19 13:21:44 +0100

    feat: Update RSS data

--- src/consts.ts
+++ src/consts.ts
@@ -2,6 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'a wandering soul.';
-export const SITE_DESCRIPTION = "Hey there, I'm Jono Yeong 👋. \
-				I'm a Staff Software Engineer from Australia currently based in Canada. \
+export const SITE_DESCRIPTION = "Hey there, I'm Bach Le 👋. \
+				I'm a Software Developer from Vietnam currently based in Germany. \
 				This blog is a collection of notes on topics that I find interesting and exciting.";
